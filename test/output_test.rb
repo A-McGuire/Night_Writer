346ca40eeb442c66bd59bf7456b023e0d96c8b24
Aidan McGuire <>
2021-03-07 09:43:46 -0700

    Fix output test

--- test/output_test.rb
+++ test/output_test.rb
@@ -6,7 +6,8 @@ require './lib/output'
 class OutputTest < MiniTest::Test
 
  def test_it_exists_with_attributes
-    output = Output.new("new_file.txt", "00\n00\n..")
+  converted = ["..", "..", ".."]
+    output = Output.new("new_file.txt", converted)
     assert_instance_of Output, output
   end
 end
