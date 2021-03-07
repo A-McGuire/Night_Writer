d84bdeba7e58b8ee39709ffee15e3745ccc4ec95
Aidan McGuire <>
2021-03-07 14:58:36 -0700

    New tests for input

--- test/input_test.rb
+++ test/input_test.rb
@@ -4,24 +4,24 @@ require './lib/input'
 class InputTest < MiniTest::Test
 
   def test_it_exists_with_attributes
-    file = mock
-    input = Input.new(file)
-    assert_equal 0, input.char_count
-    assert_equal [], input.data
-  end
-  
-  def test_read_file
-    file = './sample_txt/over_40.txt'
-    input = Input.new(file)
-    input.read(file)
-    assert_equal 50, input.char_count
-    assert_equal 50, input.data.size
+    input = Input.new("string")
+    assert_equal Array, input.data.class
+    assert_equal ["s", "t", "r", "i", "n", "g"], input.data
   end
   
   def test_chunk_data
-    file = mock
-    input = Input.new(file)
-    input.stubs(:data).returns(['a', 'b', 'c', 'd'])
+    input = Input.new("abcd")
+    input2 = Input.new("ab cd")
+
     assert_equal ['ab', 'cd'], input.chunk_data(2)
+    assert_equal ['ab', ' c', "d"], input2.chunk_data(2)
+  end
+  
+  def test_char_count
+    input = Input.new("abcd")
+    input2 = Input.new("ab cd")
+
+    assert_equal 4, input.char_count
+    assert_equal 5, input2.char_count
   end
 end
