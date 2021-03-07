d63fafdba4063f03e7b1e14e6f5dd3ec4e8be8d3
Aidan McGuire <>
2021-03-07 16:15:47 -0700

    Write new test for char_count and format methods

--- test/reader_test.rb
+++ test/reader_test.rb
@@ -5,9 +5,20 @@ require './lib/reader'
 class ReaderTest < MiniTest::Test
 
  def test_it_exists_with_attributes
-    reader = Reader.new("..")
+    reader = Reader.new([".."])
     
     assert_instance_of Reader, reader
-    assert_equal "..", reader.file
+    assert_equal [".."], reader.file
+  end
+  
+  def test_char_count
+    reader = Reader.new(["00\n", "00\n", ".."])
+
+    assert_equal 6, reader.char_count
+  end
+  
+  def test_format
+    reader = Reader.new(["00\n", "00\n", ".."])
+    assert_equal ["00", "00", ".."], reader.format
   end
 end
