897b13e71f66a6731ba1d6e06d841a4b1e1fcbe6
Aidan McGuire <>
2021-03-07 18:03:22 -0700

    Add tests for reader helpers

--- test/reader_test.rb
+++ test/reader_test.rb
@@ -16,9 +16,22 @@ class ReaderTest < MiniTest::Test
 
     assert_equal 6, reader.char_count
   end
-  
-  def test_format
+
+  def test_format_one_letter
     reader = Reader.new(["00\n", "00\n", ".."])
-    assert_equal ["00", "00", ".."], reader.format
+    
+    assert_equal [["0", "0"], ["0", "0"], [".", "."]], reader.format
+  end
+  
+  def test_to_converter_format_one_letter
+    reader = Reader.new(["00\n", "00\n", ".."])
+    reader.format
+    assert_equal [["00", "00", ".."]], reader.to_converter_format
+  end
+  
+  def test_to_english_one_letter
+    reader = Reader.new(["00\n", "00\n", ".."])
+    reader.format
+    assert_equal "g",  reader.to_english
   end
 end
