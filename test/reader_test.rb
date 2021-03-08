0ec8da3026ddbfb0c23d1b4e57f564a4dd415a3d
Aidan McGuire <>
2021-03-08 10:24:01 -0700

    Refactor reader with helpers, add tests for helpers

--- test/reader_test.rb
+++ test/reader_test.rb
@@ -25,13 +25,38 @@ class ReaderTest < MiniTest::Test
   
   def test_to_converter_format_one_letter
     reader = Reader.new(["00\n", "00\n", ".."])
+
     reader.format
+
     assert_equal [["00", "00", ".."]], reader.to_converter_format
   end
   
   def test_to_english_one_letter
     reader = Reader.new(["00\n", "00\n", ".."])
+
     reader.format
+
     assert_equal "g",  reader.to_english
+  end
+  
+  def test_create_groups_by_3
+    reader = Reader.new(["00\n", "00\n", ".."])
+
+    expected = {1=>[1], 2=>[2], 0=>[3]}
+
+    assert_equal expected, reader.create_groups_by_3
+  end
+  
+  def test_condense_to_letters
+    file = ["0.\n", "..\n", "..\n", "0.\n", "..\n", ".."]
+    reader = Reader.new(file)
+
+    reader.format
+    reader.create_groups_by_3
+    line1 = ["0.", "0."]
+    line2 = ["..", ".."]
+    line3 = ["..", ".."]
+    assert_equal [["0.", "..", ".."], ["0.", "..", ".."]],
+    reader.condense_to_letters(line1, line2, line3)
   end
 end
