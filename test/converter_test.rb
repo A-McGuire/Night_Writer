4d36d13ae24bfdc53ac50c91e7ce45c28100ea44
Aidan McGuire <>
2021-03-07 17:17:50 -0700

    Add test for one letter braille to english

--- test/converter_test.rb
+++ test/converter_test.rb
@@ -8,15 +8,19 @@ class ConverterTest < MiniTest::Test
     assert_instance_of Converter, converter
   end
 
-  def test_it_converts_one_letter
+  def test_it_converts_one_letter_to_braille
     assert_equal ["00", "00", ".."], Converter.convert_to_braille("g")
   end
   
-  def test_it_converts_one_word
+  def test_it_converts_one_word_to_braille
     assert_equal ["00.00.0.0.", "0.0..0.0..", "0...0000.."], Converter.convert_to_braille("pizza")
   end
   
-  def test_it_converts_two_words
+  def test_it_converts_two_words_to_braille
     assert_equal ["0.0...0.0.", "......0.0.", ".........."], Converter.convert_to_braille("aa bb")
+  end
+
+  def test_it_converts_one_letter_to_english_char
+    assert_equal "g", ["00", "00", ".."]
   end
 end
