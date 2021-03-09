327d93e5f5e83b47d9c51ecdef22c9d7d9e73d47
Aidan McGuire <>
2021-03-08 17:57:28 -0700

    refactor to converter format

--- lib/sanitize_braille.rb
+++ lib/sanitize_braille.rb
@@ -9,45 +9,53 @@ class SanitizeBraille
   def to_english
     english = Converter.convert_to_english(to_converter_format)
     if english.size > 80 
-      english.scan(/.{1,80} /).join("\n") #adds newline every 80 characters
+      english.scan(/.{1,80}/).join("\n") #adds newline every 80 characters
     else 
       english
     end
   end
   
   def to_converter_format
-    line1 = []
-    line2 = []
-    line3 = []
+    test = @file.map do |line|
+      line.scan(/.{2}/)
+    end
     create_groups_by_3 # creates hash that associates lines that 
-                       # share the same index of a braille character
-    @groups.values[0].each do |value| #groups.values[0] == 1, 4 which represent line numbers
-      breakdown[value - 1].each_slice(2) do |letter| #value - 1 to account for index 0
-        line1 << letter.join #creating an array that contains all 'top' character sections, 
-      end                    # with each char section as an element
-    end
-    @groups.values[1].each do |value|
-      breakdown[value - 1].each_slice(2) do |letter|
-        line2 << letter.join
-      end
-    end
-    @groups.values[2].each do |value|
-      breakdown[value - 1].each_slice(2) do |letter|
-        line3 << letter.join
-      end
-    end
+    
+    line1 = test.values_at(0, 3, 6).compact!.flatten
+    line2 = test.values_at(1, 4, 7).compact!.flatten
+    line3 = test.values_at(2, 5, 8).compact!.flatten
+    # line1 = []
+    # line2 = []
+    # line3 = []
+
+    # share the same index of a braille character
+    # @groups.values[0].each do |value| #groups.values[0] == 1, 4 which represent line numbers
+    #   breakdown[value - 1].each_slice(2) do |letter| #value - 1 to account for index 0
+    #     line1 << letter.join #creating an array that contains all 'top' character sections, 
+    #   end                    # with each char section as an element
+    # end
+    # @groups.values[1].each do |value|
+    #   breakdown[value - 1].each_slice(2) do |letter|
+    #     line2 << letter.join
+    #   end
+    # end
+    # @groups.values[2].each do |value|
+    #   breakdown[value - 1].each_slice(2) do |letter|
+    #     line3 << letter.join
+    #   end
+    # end
     condense_to_letters(line1, line2, line3) # creates a nested array where each element is a horizontal braille letter 
   end
 
-  def breakdown #removes newlines and formats data into an array of inidividual chars/dots
-    file.map do |ele|
-      ele.chomp!
-      ele.chars
-    end
-  end
+  # def breakdown #removes newlines and formats data into an array of inidividual chars/dots
+  #   file.map do |ele|
+  #     ele.chomp!
+  #     ele.chars
+  #   end
+  # end
   
   def create_groups_by_3 # creates hash that associates lines that share the same index of a braille character
-    @groups = (1..breakdown.length).group_by do |i|
+    @groups = (1..@file.length).group_by do |i|
       i % 3
     end
   end
