612c3c5d0cfab04482fcfcbdf7bfbe910df9b3e5
Aidan McGuire <>
2021-03-08 19:42:18 -0700

    Refactor test to reflect changes to method

--- test/sanitize_braille_test.rb
+++ test/sanitize_braille_test.rb
@@ -26,23 +26,44 @@ class SanitizeBrailleTest < MiniTest::Test
   def test_to_converter_breakdown_one_letter
     sanitize_braille = SanitizeBraille.new(["00\n", "00\n", ".."])
 
-    sanitize_braille.breakdown
-
+    assert_equal [["00", "00", ".."]], sanitize_braille.to_converter_format
+  end
+  
+  def test_to_converter_format_large
+    sanitize_braille = SanitizeBraille.new(["00\n", "00\n", ".."])
+  
     assert_equal [["00", "00", ".."]], sanitize_braille.to_converter_format
   end
   
   def test_to_english_one_letter
     sanitize_braille = SanitizeBraille.new(["00\n", "00\n", ".."])
 
-    sanitize_braille.breakdown
-
     assert_equal "g",  sanitize_braille.to_english
+  end
+  
+  def test_to_english_large
+    input = [
+      "0.0.0.0...0.0.0.0...00000000..00000000..0.0.0.0...00000000..00000000..0.0.0.0...\n",
+      "..........0.0.0.0..............0.0.0.0...0.0.0.0..0.0.0.0...00000000..00000000..\n",
+      "................................................................................\n",
+      ".0.0.0.0...0.0.0.0..0.0.0.0...0.0.0.0...00000000..00000000..0.0.0.0...00000000..\n",
+      "0.0.0.0...00000000............0.0.0.0..............0.0.0.0...0.0.0.0..0.0.0.0...\n",
+      "................................................................................\n",
+      "00000000..0.0.0.0....0.0.0.0...0.0.0.0..\n",
+      "00000000..00000000..0.0.0.0...00000000..\n",
+      "........................................\n"]
+    expected = "aaaa bbbb cccc dddd eeee ffff gggg hhhh iiii jjjj aaaa bbbb cccc dddd eeee ffff 
+gggg hhhh iiii jjjj "
+
+    sanitize_braille = SanitizeBraille.new(input)
+  
+    assert_equal expected,  sanitize_braille.to_english
   end
   
   def test_create_groups_by_3
     sanitize_braille = SanitizeBraille.new(["00\n", "00\n", ".."])
 
-    expected = {1=>[1], 2=>[2], 0=>[3]}
+    expected = {1=>[0], 2=>[1], 0=>[2]}
 
     assert_equal expected, sanitize_braille.create_groups_by_3
   end
@@ -51,7 +72,6 @@ class SanitizeBrailleTest < MiniTest::Test
     file = ["0.\n", "..\n", "..\n", "0.\n", "..\n", ".."]
     sanitize_braille = SanitizeBraille.new(file)
 
-    sanitize_braille.breakdown
     sanitize_braille.create_groups_by_3
     line1 = ["0.", "0."]
     line2 = ["..", ".."]
